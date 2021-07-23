db0a36f7ec2e734bf9c91639805b707961ac6fbe
Crystian Lefundes <crystian.lf@gmail.com>
2021-07-23 16:19:09 -0300

    Create the HandleCart function with the localStorage

--- src/components/ProductsCard/index.js
+++ src/components/ProductsCard/index.js
@@ -6,20 +6,28 @@ import bag from '../../images/shopping-bag.svg'
 
 export default function ProductsCard() {
     const [ data, setData ] = useState([])
+    const cart = []
     useEffect(() => {
         api.get('').then(response => {
             setData(response.data)
         })
     },[])
+
+    function handleCart(index) {
+        const productStorage = JSON.stringify(data[index])
+        const pushCart = [...cart, cart.push(productStorage)]
+        localStorage.setItem(`@Cart`,pushCart)
+    }
+
     return (
         <>
-            {data.map(product => {
+            {data.map((product,index) => {
                 return (
                 <S.Card key={product.id}>
                     <img src={product.photo} alt="" />
                     <S.ProductName>{product.name}</S.ProductName>
                     <S.Price>R$ {product.price},00</S.Price>
-                    <S.Button><img src={bag} alt=""/>COMPRAR</S.Button>
+                    <S.Button onClick={() => handleCart(index)}><img src={bag} alt="ícone de sacola"/>COMPRAR</S.Button>
                     
                 </S.Card>
                 )
